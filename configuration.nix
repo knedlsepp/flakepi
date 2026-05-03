f99e5deb6d4b89aab0b6654f8d5acf41f5ebc7b0
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2026-05-03 07:20:21 +0200

    wip

--- configuration.nix
+++ configuration.nix
@@ -59,6 +59,7 @@
                 "lightning_strike"
                 "negroni_code"
                 "spinout"
+                # "star_hit" # FIXME: if player hits CPU => issue # {"event":"star_hit", "ownerIndex":2, "playerIndex":0, "isHumanOwner":false, "isHumanPlayer":true}
               )
 
               isAllowlisted() {
