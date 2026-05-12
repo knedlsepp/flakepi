8aec3e50c4a9af76575cbdf56c6e7bedeab3b265
Josef Kemetmüller <josef.kemetmueller@gmail.com>
2026-05-12 22:41:13 +0200

    Merge drink-dispenser service with sc64deployer
    We don't want to go via the journal. That was kinda stupid.

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,7 @@
 
   systemd.services.sc64deployer = {
     enable = true;
-    description = "sc64deployer ROM Upload Service";
+    description = "sc64deployer ROM Upload Service with drink dispenser";
     wantedBy = [ "multi-user.target" ];
     unitConfig = {
       StartLimitIntervalSec = "0";
@@ -12,91 +12,74 @@
     serviceConfig = {
       Type = "simple";
       ExecStart =  let
-          upload-rom = pkgs.writeShellApplication {
-            name = "upload-rom";
-            runtimeInputs = [
-              pkgs.sc64deployer
-            ];
-            text = ''
-              until sc64deployer list; do sleep 2; done
-              sc64deployer upload /var/lib/sc64deployer/build.rom
-              # Prevent "[IS-Viewer 64]: Stopped listening" via 'tail -f /dev/null'
-              tail -f /dev/null | sc64deployer debug --isv 0x03FF0000
-            '';
-          };
-        in
-        "${upload-rom}/bin/upload-rom";
+        upload-rom = pkgs.writeShellApplication {
+          name = "upload-rom";
+          runtimeInputs = [ pkgs.sc64deployer ];
+          text = ''
+            until sc64deployer list; do sleep 2; done
+            sc64deployer upload /var/lib/sc64deployer/build.rom
+            # Prevent "[IS-Viewer 64]: Stopped listening" via 'tail -f /dev/null'
+            exec tail -f /dev/null | sc64deployer debug --isv 0x03FF0000
+          '';
+        };
+
+        drink-dispenser = pkgs.writeShellApplication {
+          name = "drink-dispenser";
+          runtimeInputs = [ pkgs.jq pkgs.libgpiod ];
+          text = ''
+            playerToGPIO=(5 6 13 16 19 20 21 26)
+
+            allowlist=(
+              "hit_banana"
+              "explosion_crash"
+              "terrain_tumble"
+              "hit_paddle_boat"
+              "squished"
+              "fell_in_lava"
+              "fell_in_water"
+              "high_tumble"
+              "hit_by_star"
+              "lightning_strike"
+              "low_tumble"
+              "negroni_code"
+              "spinout"
+              "driving_spinout"
+              "early_start_spinout"
+              # "star_hit" # FIXME: if player hits CPU => issue # {"event":"star_hit", "ownerIndex":2, "playerIndex":0, "isHumanOwner":false, "isHumanPlayer":true}
+            )
+
+            isAllowlisted() {
+              local e="$1"
+              for w in "''${allowlist[@]}"; do
+                [[ "$e" == "$w" ]] && return 0
+              done
+              return 1
+            }
+
+            while IFS= read -r line
+            do
+              echo "$line" | jq -e . >/dev/null 2>&1 || continue
+              echo "Got event $line"
+
+              event=$(echo "$line" | jq -r '.event // empty') || continue
+              playerIndex=$(echo "$line" | jq -r '.playerIndex // empty') || continue
+
+              isAllowlisted "$event" || continue
+
+              if [ -n "$playerIndex" ]; then
+                echo "Dispensing drink for Player #$playerIndex (event: $event)"
+                gpioset -c 0 -l -t 1500ms,0s "''${playerToGPIO[$playerIndex]}=1" &
+              fi
+            done
+          '';
+        };
+      in
+        "${pkgs.bash}/bin/bash -c '${upload-rom}/bin/upload-rom |& ${drink-dispenser}/bin/drink-dispenser'";
 
       User = "sc64deployer";
       Group = "sc64deployer";
       Restart = "always";
       RestartSec = "1";
-    };
-  };
-
-  systemd.services.drink-dispenser = {
-    enable = true;
-    description = "Dispense drinks based on the logs from the SC64";
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      Type = "simple";
-      ExecStart =
-        let
-          drink-dispenser = pkgs.writeShellApplication {
-            name = "drink-dispenser";
-            runtimeInputs = [
-              pkgs.jq
-              pkgs.libgpiod
-            ];
-            text = ''
-              playerToGPIO=(5 6 13 16 19 20 21 26)
-
-              allowlist=(
-                "hit_banana"
-                "explosion_crash"
-                "terrain_tumble"
-                "hit_paddle_boat"
-                "squished"
-                "fell_in_lava"
-                "fell_in_water"
-                "high_tumble"
-                "hit_by_star"
-                "lightning_strike"
-                "low_tumble"
-                "negroni_code"
-                "spinout"
-                "driving_spinout"
-                "early_start_spinout"
-                # "star_hit" # FIXME: if player hits CPU => issue # {"event":"star_hit", "ownerIndex":2, "playerIndex":0, "isHumanOwner":false, "isHumanPlayer":true}
-              )
-
-              isAllowlisted() {
-                local e="$1"
-                for w in "''${allowlist[@]}"; do
-                  [[ "$e" == "$w" ]] && return 0
-                done
-                return 1
-              }
-
-              journalctl --since now -f -u sc64deployer -o cat | while IFS= read -r line
-              do
-                echo "$line" | jq -e . >/dev/null 2>&1 || continue
-
-                event=$(echo "$line" | jq -r '.event // empty') || continue
-                playerIndex=$(echo "$line" | jq -r '.playerIndex // empty') || continue
-
-                isAllowlisted "$event" || continue
-
-                if [ -n "$playerIndex" ]; then
-                  echo "Dispensing drink for Player #$playerIndex (event: $event)"
-                  gpioset -c 0 -l -t 1500ms,0s "''${playerToGPIO[$playerIndex]}=1" &
-                fi
-              done
-            '';
-          };
-        in
-        "${drink-dispenser}/bin/drink-dispenser";
-      Restart = "always";
     };
   };
 
@@ -111,10 +94,15 @@
     group = "sc64deployer";
     extraGroups =  [
       "dialout" # Access to serial device
+      "gpio"    # Access to GPIO devices
     ];
   };
 
   users.groups.sc64deployer = { };
+  users.groups.gpio = { };
+  services.udev.extraRules = ''
+      KERNEL=="gpiochip[0-9]*", GROUP="gpio", MODE="0660"
+  '';
 
 
   services.openssh.enable = true;
